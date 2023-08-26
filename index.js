0a425249cf1dda7092bec3971af82d68b9618da0
MEGHAN99 <114795563+MEGHAN99@users.noreply.github.com>
2023-08-26 15:26:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+ let randomNumber1 = Math.floor(Math.random() * 6)+ 1;
+ let randomImg = "dice"+randomNumber1+".png";
+ let randomSrc = "images/"+randomImg;
+ 
+ let img1=document.querySelectorAll("img")[0].setAttribute("src",randomSrc);
+
+
+
+ let randomNo2 = Math.floor(Math.random()*6)+1;
+ let randomImg1 = "dice"+randomNo2+".png";
+ let randomsrc2 = "images/"+randomImg1;
+
+ let img2 = document.querySelectorAll("img")[1].setAttribute("src",randomsrc2);
+let lucky = "LUCY day";
+ if(randomNumber1===randomNo2){
+document.querySelector("h1").innerText=("LUCKY day");
+alert("Looks like a LUCKY day")
+}
+else{
+    document.querySelector("h1").innerText=("not LUCKY");
+    
+}
